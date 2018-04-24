13656563af1357cd2e3389cae03f337227e36e81
CCDM23 <ccdm23@CCDM23.local>
2018-04-24 12:55:24 -0600

    Primer JQuerty

---
+++ js/primerJQuery.js
@@ -0,0 +1,25 @@
+// jquery = $
+var inicia = function(){
+
+	var nuevo = function(){
+		$.ajax({
+		  url: 'https://randomuser.me/api/',
+		  dataType: 'json',
+		  success: function(data) {
+		  	$("#nombre").html(data.results[0].name.first + " " + data.results[0].name.last);
+
+		  	$("#foto").attr("src",data.results[0].picture.large);
+  		  }
+		});
+	}
+
+	$("#btnNuevo").on("click",nuevo);
+	//$("#btnNuevo").off("click", nuevo);
+}
+
+$(document).ready(inicia);
+
+
+
+
+
