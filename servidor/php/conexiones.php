de4a53e3b92189b5a57e3fc7f1617623e07e1080
victor <13170333@itculiacan.edu.mx>
2018-05-23 13:06:10 -0600

    Cosas

--- servidor/php/conexiones.php
+++ servidor/php/conexiones.php
@@ -1,7 +1,7 @@
 <?php 
 
 function conecta(){
-	$con = mysql_connect("127.0.0.1","root","","pw218112");
+	$con = mysqli_connect("127.0.0.1","root","","pw218112");
 	return $con;
 }
 
--- servidor/php/validaentrada.php
+++ servidor/php/validaentrada.php
@@ -6,8 +6,8 @@
 		//CONECTARNOS AL SERVIDOR DE BD.
 		$con = conecta();
 		$consulta = "select * from usuario where usuario='".$usuario."' and clave='".$clave."' limit 1";
-		$resConsulta=mysql_query($con, $consulta);
-		if (mysql_num_rows($resConsulta) > 0) {
+		$resConsulta=mysqli_query($con, $consulta);
+		if (mysqli_num_rows($resConsulta) > 0) {
 
 			$respuesta = true;
 		}
